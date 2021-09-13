f6220f12f5d71a1b67e99de8c1e586d377498b6d
Nico <ainililia@163.com>
2021-09-14 01:10:35 +0800

    tx

--- src/main/java/io/openmessaging/impl/MessageQueueImpl.java
+++ src/main/java/io/openmessaging/impl/MessageQueueImpl.java
@@ -33,10 +33,10 @@ public class MessageQueueImpl extends MessageQueue {
                 "/essd/",
                 "/pmem/nico",
                 Const.G * 59,
-                (int) (Const.M * 64),
-                (int) (Const.G * 45 / (Const.M * 64)),
+                (int) (Const.M * 4),
+                (int) (Const.G * 30 / (Const.M * 4)),
                 10,
-                10000)
+                5000)
         );
     }
 
