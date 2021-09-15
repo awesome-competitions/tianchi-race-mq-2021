899b053106f11160ed514bb35f054fb97c92be84
Nico <ainililia@163.com>
2021-09-15 16:26:38 +0800

    ok?

--- src/main/java/io/openmessaging/impl/MessageQueueImpl.java
+++ src/main/java/io/openmessaging/impl/MessageQueueImpl.java
@@ -34,8 +34,8 @@ public class MessageQueueImpl extends MessageQueue {
                 "/essd/",
                 "/pmem/nico",
                 Const.G * 60,
-                Const.K * 288,
-                200)
+                Const.K * 300,
+                100)
         );
     }
 
@@ -81,10 +81,10 @@ public class MessageQueueImpl extends MessageQueue {
             if (count % 100000 == 0){
                 LOGGER.info("write count {}, size {}, topic size{}", count, size, topics.size());
             }
-            if (count > 500000){
-                LOGGER.info("stop count {}, size {}", count, size);
-                throw new RuntimeException("stop");
-            }
+//            if (count > 1000000){
+//                LOGGER.info("stop count {}, size {}", count, size);
+//                throw new RuntimeException("stop");
+//            }
             return getTopic(topic).write(queueId, data);
         } catch (IOException e) {
             e.printStackTrace();
