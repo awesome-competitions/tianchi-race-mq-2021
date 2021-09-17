41bdd864e7971c658447eeed66c067fd42819003
Nico <ainililia@163.com>
2021-09-17 15:39:19 +0800

    一个会快？

--- src/main/java/io/openmessaging/impl/MessageQueueImpl.java
+++ src/main/java/io/openmessaging/impl/MessageQueueImpl.java
@@ -79,10 +79,10 @@ public class MessageQueueImpl extends MessageQueue {
             if (count % 100000 == 0){
                 LOGGER.info("write count {}, size {}, topic size{}", count, size, topics.size());
             }
-//            if (count > 1000000){
-//                LOGGER.info("stop count {}, size {}", count, size);
-//                throw new RuntimeException("stop");
-//            }
+            if (count > 1000000){
+                LOGGER.info("stop count {}, size {}", count, size);
+                throw new RuntimeException("stop");
+            }
             return getTopic(topic).write(queueId, data);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -118,6 +118,7 @@ public class MessageQueueImpl extends MessageQueue {
     }
 
     public synchronized Topic getTopic(String name) throws IOException {
+        name = "ababa";
         Topic topic = topics.get(name);
         if (topic == null){
             topic = new Topic(name, id.getAndIncrement() * 10000, config, cache);
