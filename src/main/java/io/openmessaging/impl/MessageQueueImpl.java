c4f1ab34c8e853a626bf434d69005ce1c061dd6c
Nico <ainililia@163.com>
2021-09-21 18:42:55 +0800

    fx

--- src/main/java/io/openmessaging/impl/MessageQueueImpl.java
+++ src/main/java/io/openmessaging/impl/MessageQueueImpl.java
@@ -91,7 +91,7 @@ public class MessageQueueImpl extends MessageQueue {
             if (count % 100000 == 0){
                 LOGGER.info("write count {}, size {}, topic size{}", count, size, topics.size());
             }
-            if (count > 100000){
+            if (count > 1000000){
                 LOGGER.info("stop count {}, size {}", count, size);
                 throw new RuntimeException("stop");
             }
