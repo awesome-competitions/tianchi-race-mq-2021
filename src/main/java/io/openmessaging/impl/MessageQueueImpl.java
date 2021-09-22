21277a5aa5b899b4f652a46c76ad805476468577
Nico <ainililia@163.com>
2021-09-22 15:07:46 +0800

    fix

--- src/main/java/io/openmessaging/impl/MessageQueueImpl.java
+++ src/main/java/io/openmessaging/impl/MessageQueueImpl.java
@@ -35,8 +35,8 @@ public class MessageQueueImpl extends MessageQueue {
                 (int) ((Const.G * 51) / (Const.K * 288)),
                 Const.K * 288,
                 1,
-                35,
-                Const.K * 128)
+                40,
+                Const.K * 256)
         );
     }
 
