8397917631baf90ef7b5c0c9e01271fb91ce444a
Nico <ainililia@163.com>
2021-09-22 17:12:28 +0800

    pingjing@?

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -47,7 +47,7 @@ public class DefaultMessageQueueImpl extends MessageQueue{
         RandomAccessFile randomAccessFile = new RandomAccessFile("/essd/aof.log", "rw");
         FileChannel channel = randomAccessFile.getChannel();
 
-        int batch = (int) (Const.K * 448);
+        int batch = (int) (Const.K * 1024);
         int count = (int) (Const.G * 10 / batch);
 
         ByteBuffer buffer = ByteBuffer.allocate(batch);
