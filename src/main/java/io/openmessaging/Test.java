755aaa7e050db22dd2bfd7323edc7b526f8f6059
Nico <ainililia@163.com>
2021-09-22 16:48:00 +0800

    lipu

--- src/main/java/io/openmessaging/Test.java
+++ src/main/java/io/openmessaging/Test.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
@@ -17,25 +18,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Test {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        Aof aof = new Aof(new FileWrapper(new RandomAccessFile("D:\\test\\nio\\abc.txt", "rw")), new Config(
-                "", "", 1L ,1 ,1L,1 ,3, 3
-        ));
-
-        for (int n = 0; n < 3; n ++){
-            new Thread(()->{
-                for (int i = 0; i < 100; i ++){
-                    try {
-                        aof.write(ByteBuffer.wrap(new byte[]{1}));
-                        System.out.println(i);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-
-
-        Thread.sleep(10000000);
+        System.out.println(Arrays.toString("Xfaewrq1313".getBytes()));
     }
 
 }
