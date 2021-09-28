21fff6b66f7d7cd54e0b9e9b46e81f0f66ef90a9
Nico <ainililia@163.com>
2021-09-28 23:12:58 +0800

    dd

--- src/main/java/io/openmessaging/mq/Cache.java
+++ src/main/java/io/openmessaging/mq/Cache.java
@@ -8,37 +8,18 @@ public class Cache {
 
     private Heap heap;
 
-    private final AtomicLong used;
-
-    private final Config config;
-
     public Cache(Config config){
         if (config.getHeapDir() != null){
             this.heap = Heap.exists(config.getHeapDir()) ? Heap.openHeap(config.getHeapDir()) : Heap.createHeap(config.getHeapDir(), config.getHeapSize());
         }
-        this.config = config;
-        this.used = new AtomicLong();
     }
 
     public Data apply(int capacity){
         if (heap == null){
             return new Dram(capacity);
         }
-        if (used.addAndGet(capacity) < config.getHeapUsableSize()){
-            Monitor.heapUsedSize = used.longValue();
-            return new PMem(heap.allocateCompactMemoryBlock(capacity), capacity);
-        }else{
-            used.addAndGet(- capacity);
-        }
-        return null;
-    }
-
-    public void recycle(Data data){
-        if (heap != null && data instanceof PMem){
-            data.clear();
-            used.addAndGet(- data.getCapacity());
-            Monitor.heapUsedSize = used.longValue();
-        }
+        Monitor.heapUsedSize += capacity;
+        return new PMem(heap.allocateCompactMemoryBlock(capacity), capacity);
     }
 
 }
