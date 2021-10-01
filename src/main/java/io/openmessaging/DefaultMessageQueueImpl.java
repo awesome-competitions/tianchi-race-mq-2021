e191b7af50f1a006b81e82106cef39064568e2c4
Nico <ainililia@163.com>
2021-10-01 21:18:05 +0800

    1

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -31,7 +31,7 @@ public class DefaultMessageQueueImpl extends MessageQueue{
             Const.G * 59,
             Const.G * 54,
             40,
-            (int) (Const.M * 4),
+            (int) (Const.K * 256),
             Const.MINUTE * 15 + Const.SECOND * 2
     ));
 //    private final MessageQueue queue = null;
