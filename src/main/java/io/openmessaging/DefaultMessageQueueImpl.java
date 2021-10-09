c8625797b5692ca97adebcb839024ccd5c4a539c
Nico <ainililia@163.com>
2021-10-09 20:02:04 +0800

    8A

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -29,7 +29,7 @@ public class DefaultMessageQueueImpl extends MessageQueue{
             "/essd/",
             "/pmem/nico",
             Const.G * 60,
-            4,
+            8,
             (int) (Const.K * 256),
             Const.MINUTE * 1 + Const.SECOND * 2
     ));
