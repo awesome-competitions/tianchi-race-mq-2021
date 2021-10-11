6e984efc4d2f100e0726bbbec1ba190c9940cb4f
Nico <ainililia@163.com>
2021-10-11 12:49:22 +0800

    够应该

--- src/main/java/io/openmessaging/mq/Buffers.java
+++ src/main/java/io/openmessaging/mq/Buffers.java
@@ -43,7 +43,7 @@ public class Buffers {
         for (int i = 0; i < 80000; i ++){
             readBuffers.add(new Dram(ByteBuffer.allocateDirect((int) (Const.K * 17))));
         }
-        for (int i = 0; i < 150000; i ++){
+        for (int i = 0; i < 200000; i ++){
             readBuffers.add(new Dram(ByteBuffer.allocate((int) (Const.K * 17))));
         }
     }
