d2cc4973cffded269ad9cc29a60c29338d07a930
Nico <ainililia@163.com>
2021-10-14 20:04:07 +0800

    2w试试吧

--- src/main/java/io/openmessaging/mq/Buffers.java
+++ src/main/java/io/openmessaging/mq/Buffers.java
@@ -9,7 +9,7 @@ public class Buffers {
 
     public static final LinkedBlockingQueue<ByteBuffer> AEP_BUFFERS = new LinkedBlockingQueue<>();
 
-    public static final int MAX_SIZE = 140000;
+    public static final int MAX_SIZE = 100000;
     public static final int THRESHOLD_SIZE = MAX_SIZE - 1000;
 
     public static Data allocateReadBuffer(){
