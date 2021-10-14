ee0a84e2d7e27c5535fcdafded5e6b89c1c22f94
Nico <ainililia@163.com>
2021-10-14 13:51:08 +0800

    575

--- src/main/java/io/openmessaging/mq/Mq.java
+++ src/main/java/io/openmessaging/mq/Mq.java
@@ -104,7 +104,7 @@ public class Mq extends MessageQueue{
     }
 
     void initPools() throws IOException {
-        int[] arr = new int[]{5,5,5,5,5,5,5,5};
+        int[] arr = new int[]{7,7,7,7,7,5};
         for (int i = 0; i < arr.length; i ++){
             Barrier barrier = new Barrier(arr[i], createAof("aof" + i), cache.getBlock());
             for (int j = 0; j < arr[i]; j ++){
