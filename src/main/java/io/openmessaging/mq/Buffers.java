c9304be7d987810fe26ddfb2951cd36afb57a9a6
Nico <ainililia@163.com>
2021-10-14 17:07:41 +0800

    就这样，再试试

--- src/main/java/io/openmessaging/mq/Buffers.java
+++ src/main/java/io/openmessaging/mq/Buffers.java
@@ -10,9 +10,12 @@ public class Buffers {
     public static final LinkedBlockingQueue<ByteBuffer> AEP_BUFFERS = new LinkedBlockingQueue<>();
 
     public static Data allocateReadBuffer(){
-        if (Monitor.writeDramCount < 60000){
+        if (Monitor.writeDramCount < 80000){
             Monitor.writeDramCount ++;
             return new Dram(ByteBuffer.allocateDirect((int) (Const.K * 17)));
+        }else if (Monitor.writeDramCount < 140000){
+            Monitor.writeDramCount ++;
+            return new Dram(ByteBuffer.allocate((int) (Const.K * 17)));
         }
         return null;
     }
