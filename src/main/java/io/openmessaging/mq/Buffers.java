3c61aacd330aeeefdc6436b255511d2c01978e6a
Nico <ainililia@163.com>
2021-10-15 17:07:41 +0800

    估计不行

--- src/main/java/io/openmessaging/mq/Buffers.java
+++ src/main/java/io/openmessaging/mq/Buffers.java
@@ -10,7 +10,7 @@ public class Buffers {
 
     public static final LinkedBlockingQueue<ByteBuffer> AEP_BUFFERS = new LinkedBlockingQueue<>();
 
-    public static final int MAX_SIZE = 210000;
+    public static final int MAX_SIZE = 250000;
     public static final int DIRECT_SIZE = 110000;
     public static final int THRESHOLD_SIZE = MAX_SIZE - 50;
 
