1ef61e3710d9f06908a253f88312f586667781b9
Nico <ainililia@163.com>
2021-10-17 23:37:05 +0800

    上大分？

--- src/main/java/io/openmessaging/mq/Mq.java
+++ src/main/java/io/openmessaging/mq/Mq.java
@@ -70,8 +70,8 @@ public class Mq extends MessageQueue{
             count ++;
         }
         if (count == 0){
-            int batch = (int) (Const.K * 256);
-            int size = (int) (Const.G * 50 / batch);
+            int batch = (int) (Const.K * 512);
+            int size = (int) (Const.G * 40 / batch);
             ByteBuffer buffer = ByteBuffer.allocateDirect(batch);
             for (int i = 0; i < batch; i ++){
                 buffer.put((byte) 0);
