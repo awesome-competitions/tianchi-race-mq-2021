014b519bc7fb017f5d53236b7ff75735b2cde568
Nico <ainililia@163.com>
2021-10-18 01:09:27 +0800

    为什么总超时

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -31,7 +31,7 @@ public class DefaultMessageQueueImpl extends MessageQueue{
             Const.G * 60,
             40,
             6,
-            Const.SECOND * 650
+            Const.SECOND * 1200
     ));
 //    private final MessageQueue queue = null;
 
