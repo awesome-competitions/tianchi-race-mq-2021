5f36b54123c0ac1521eca3c5b8f579cbd0ed8f53
Nico <ainililia@163.com>
2021-10-18 01:35:57 +0800

    有这么慢吗？

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -31,7 +31,7 @@ public class DefaultMessageQueueImpl extends MessageQueue{
             Const.G * 60,
             40,
             6,
-            Const.SECOND * 1200
+            Const.SECOND * 3000
     ));
 //    private final MessageQueue queue = null;
 
--- src/main/java/io/openmessaging/mq/Mq.java
+++ src/main/java/io/openmessaging/mq/Mq.java
@@ -123,28 +123,17 @@ public class Mq extends MessageQueue{
     }
 
 
-    void initPools() throws IOException {
+    void initPools() {
         int[] arr = new int[]{10,10,10,10};
-        CountDownLatch cdl = new CountDownLatch(arr.length);
         for (int i = 0; i < arr.length; i ++){
-            final int index = i;
-            new Thread(()->{
-                try {
-                    Barrier barrier = new Barrier(arr[index], createAof("aof" + index), block);
-                    for (int j = 0; j < arr[index]; j ++){
-                        POOLS.add(barrier);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }finally {
-                    cdl.countDown();
+            try {
+                Barrier barrier = new Barrier(arr[i], createAof("aof" + i), block);
+                for (int j = 0; j < arr[i]; j ++){
+                    POOLS.add(barrier);
                 }
-            }).start();
-        }
-        try {
-            cdl.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
